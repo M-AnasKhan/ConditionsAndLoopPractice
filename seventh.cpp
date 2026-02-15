ed4bb1078e4d4371e3fcaadf9dfb102c6b9a7a9a
M-AnasKhan <wahmiooooo7@gmail.com>
2026-02-15 09:34:43 +0500

    Divisible by 5 and 11

---
+++ seventh.cpp
@@ -0,0 +1,25 @@
+// Check if a number is divisible by 5 and 11.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num ;
+
+	cout << " Enter a number " ;
+	cin >>  num;
+
+	if ( num % 5 == 0 && num % 11 == 0 )
+	
+	{
+		cout << num << " is divisible by 5 and 11";
+	}
+
+	else
+	{
+
+		cout << num << " is not divisible by 5 and 11";
+	}
+}
+
+	
